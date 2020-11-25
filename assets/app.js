0f86bd9cb15c4c54b77540a3405af212a6474d5c
Valentin Cernuta <vcernuta3@gmail.com>
2020-11-25 14:12:53 +0100

    Mise à jour du CSS (utilisation de CodeMirror en local) + mise à jour pour accélérer Docker (cache)

--- assets/app.js
+++ assets/app.js
@@ -7,8 +7,27 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/bootswatch.css'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
 import './styles/app.scss'
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
 require('bootstrap')
+
+import 'codemirror/mode/sql/sql'
+import CodeMirror from 'codemirror/lib/codemirror'
+
+const textarea = document.getElementById('request')
+const editor = CodeMirror.fromTextArea(textarea, {
+    mode: 'text/x-sql',
+    lineNumbers: true,
+    indentWithTabs: true,
+    smartIndent: true,
+    matchBrackets : true,
+    autofocus: true,
+})
+editor.setOption('theme', 'material')
+editor.setValue("\n".repeat(2))
+
+console.log(editor)
